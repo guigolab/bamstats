616c4e1c5c175cbed9c6ec06d14dc07d2850dc2b
Emilio Palumbo <emiliopalumbo@gmail.com>
2016-04-22 14:48:25 +0200

    Update version functions

--- version.go
+++ version.go
@@ -1,5 +1,16 @@
 package bamstats
 
+import "fmt"
+
 const (
-	Version   = "0.1.0"
+	VersionNumber      = 0.1
+	MinorVersionNumber = 0
 )
+
+func Version() string {
+	return getVersion(VersionNumber, MinorVersionNumber)
+}
+
+func getVersion(version float32, minor uint8) string {
+	return fmt.Sprintf("%.2g.%d", version, minor)
+}
