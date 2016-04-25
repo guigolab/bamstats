2d058cfc259f1f61ebc394476c6c3d5abf532d34
Emilio Palumbo <emiliopalumbo@gmail.com>
2016-04-25 17:11:57 +0200

    Update benchmark test and move them to process

--- process_test.go
+++ process_test.go
@@ -8,11 +8,17 @@ import (
 )
 
 var (
-	bam        = "data/test2.bam"
+	bam        = "data/test1.bam"
 	annotation = "data/gencode.v22.annotation.201503031.chr1.bed"
 	maxBuf     = 1000000
 	reads      = -1
 )
+
+func BenchmarkGeneral(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		bamstats.Process(bam, "", runtime.GOMAXPROCS(-1), maxBuf, reads)
+	}
+}
 
 func BenchmarkCoverage(b *testing.B) {
 	for i := 0; i < b.N; i++ {
