5ce6674b01223b7db7caada1effbf5737698c44d
Emilio Palumbo <emiliopalumbo@gmail.com>
2016-04-25 17:30:08 +0200

    Update process tests

--- process_test.go
+++ process_test.go
@@ -1,27 +1,25 @@
-package bamstats_test
+package bamstats
 
 import (
 	"runtime"
 	"testing"
-
-	"github.com/bamstats"
 )
 
 var (
-	bam        = "data/test1.bam"
-	annotation = "data/gencode.v22.annotation.201503031.chr1.bed"
-	maxBuf     = 1000000
-	reads      = -1
+	bamFile        = "data/test1.bam"
+	annotationFile = "data/gencode.v22.annotation.201503031.chr1.bed"
+	maxBuf         = 1000000
+	reads          = -1
 )
 
 func BenchmarkGeneral(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		bamstats.Process(bam, "", runtime.GOMAXPROCS(-1), maxBuf, reads)
+		Process(bamFile, "", runtime.GOMAXPROCS(-1), maxBuf, reads)
 	}
 }
 
 func BenchmarkCoverage(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		bamstats.Process(bam, annotation, runtime.GOMAXPROCS(-1), maxBuf, reads)
+		Process(bamFile, annotationFile, runtime.GOMAXPROCS(-1), maxBuf, reads)
 	}
 }
