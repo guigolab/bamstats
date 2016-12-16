1c94900c77175b400f553a47c85b1ad801995f36
Emilio Palumbo <emiliopalumbo@gmail.com>
2016-12-16 17:44:30 +0100

    Add missing file in stats package

---
+++ stats/stats.go
@@ -0,0 +1,44 @@
+package stats
+
+import (
+	"github.com/bamstats/annotation"
+	"github.com/bamstats/sam"
+)
+
+// Stats represents mapping statistics.
+type Stats interface {
+	Update(other Stats)
+	Merge(others chan Stats)
+	Collect(record *sam.Record, index *annotation.RtreeMap)
+	Finalize()
+}
+
+// StatsMap is a map of Stats instances with string keys.
+type StatsMap map[string]Stats
+
+// Merge merges instances of StatsMap
+func (sm *StatsMap) Merge(stats chan StatsMap) {
+	for s := range stats {
+		for key, stat := range *sm {
+			if otherStat, ok := s[key]; ok {
+				stat.Update(otherStat)
+			}
+		}
+	}
+}
+
+func NewStatsMap(general, coverage, uniq bool) StatsMap {
+	m := make(StatsMap)
+	if general {
+		m["general"] = NewGeneralStats()
+	}
+	if coverage {
+		m["coverage"] = NewCoverageStats()
+	}
+	if uniq {
+		cs := NewCoverageStats()
+		cs.uniq = true
+		m["coverageUniq"] = cs
+	}
+	return m
+}
