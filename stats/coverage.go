7927552cd8dd8ab897f23b7ad434025f7d7a8520
Emilio Palumbo <emiliopalumbo@gmail.com>
2018-03-14 16:00:51 +0100

    Use map for CoverageStats and prepare for general coverage

--- stats/coverage.go
+++ stats/coverage.go
@@ -1,19 +1,47 @@
 package stats
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
 	"github.com/guigolab/bamstats/annotation"
 	"github.com/guigolab/bamstats/sam"
 )
 
+// general element constants
+const (
+	Exon       = "exon"
+	Intron     = "intron"
+	Intergenic = "intergenic"
+	ExonIntron = "exonic_intronic"
+	Total      = "total"
+	Other      = "others"
+)
+
+var (
+	// allElems = []string{Exon, Intron, Intergenic, ExonIntron, Other, Total}
+	allElems = []string{
+		ExonIntron,
+		Intron,
+		Exon,
+		Intergenic,
+		Other,
+		Total,
+	}
+)
+
 // ElementStats represents mappings statistics for genomic elements
-type ElementStats struct {
-	ExonIntron uint64 `json:"exonic_intronic"`
-	Intron     uint64 `json:"intron"`
-	Exon       uint64 `json:"exon"`
-	Intergenic uint64 `json:"intergenic"`
-	Other      uint64 `json:"others"`
-	Total      uint64 `json:"total"`
-}
+type ElementStats map[string]uint64
+
+// type ElementStats struct {
+// 	ExonIntron uint64 `json:"exonic_intronic"`
+// 	Intron     uint64 `json:"intron"`
+// 	Exon       uint64 `json:"exon"`
+// 	Intergenic uint64 `json:"intergenic"`
+// 	Other      uint64 `json:"others"`
+// 	Total      uint64 `json:"total"`
+// }
 
 // CoverageStats represents genome coverage statistics for continuos, split and total mapped reads.
 type CoverageStats struct {
@@ -43,46 +71,83 @@ func (s *CoverageStats) Merge(others chan Stats) {
 
 // Finalize updates dependent counts of a CoverageStats instance.
 func (s *CoverageStats) Finalize() {
-	s.Total.ExonIntron = s.Continuous.ExonIntron + s.Split.ExonIntron
-	s.Total.Exon = s.Continuous.Exon + s.Split.Exon
-	s.Total.Intron = s.Continuous.Intron + s.Split.Intron
-	s.Total.Intergenic = s.Continuous.Intergenic + s.Split.Intergenic
-	s.Total.Other = s.Continuous.Other + s.Split.Other
-	s.Total.Total = s.Continuous.Total + s.Split.Total
+	for _, elem := range allElems {
+		s.Total[elem] = s.Continuous[elem] + s.Split[elem]
+	}
 }
 
 // Update updates all counts from another ElementsStats instance.
-func (s *ElementStats) Update(other ElementStats) {
-	s.ExonIntron += other.ExonIntron
-	s.Exon += other.Exon
-	s.Intron += other.Intron
-	s.Intergenic += other.Intergenic
-	s.Other += other.Other
-	s.Total += other.Total
+func (s ElementStats) Update(other ElementStats) {
+	for k := range other {
+		s[k] += other[k]
+	}
 }
 
-func updateCount(r *sam.Record, elems map[string]uint8, st *ElementStats) {
+// MarshalJSON implements JSON Marshaller interface
+func (s ElementStats) MarshalJSON() ([]byte, error) {
+	buf := &bytes.Buffer{}
+	buf.Write([]byte{'{', '\n'})
+	l := len(allElems)
+	for i, k := range allElems {
+		val, err := json.Marshal(s[k])
+		if err != nil {
+			return nil, err
+		}
+		fmt.Fprintf(buf, "\t\"%s\": %s", k, val)
+		if i < l-1 {
+			buf.WriteByte(',')
+		}
+		buf.WriteByte('\n')
+	}
+	buf.Write([]byte{'}', '\n'})
+	return buf.Bytes(), nil
+}
+
+func updateCount(r *sam.Record, elems map[string]uint8, st ElementStats) {
 	exons, hasExon := elems["exon"]
 	introns, hasIntron := elems["intron"]
-	st.Total++
+	st[Total]++
 	if _, isIntergenic := elems["intergenic"]; isIntergenic {
 		if len(elems) > 1 {
-			st.Other++
+			st[Other]++
 		} else {
-			st.Intergenic++
+			st[Intergenic]++
 		}
 		return
 	}
 	if hasExon && !hasIntron && exons > 0 {
-		st.Exon++
+		st[Exon]++
 		return
 	}
 	if hasIntron && !hasExon && introns > 0 {
-		st.Intron++
+		st[Intron]++
 		return
 	}
-	st.ExonIntron++
+	st[ExonIntron]++
 }
+
+// func updateCount2(r *sam.Record, elems map[string]uint8, st ElementStats) {
+// 	exons, hasExon := elems["exon"]
+// 	introns, hasIntron := elems["intron"]
+// 	st.Total++
+// 	if _, isIntergenic := elems["intergenic"]; isIntergenic {
+// 		if len(elems) > 1 {
+// 			st.Other++
+// 		} else {
+// 			st.Intergenic++
+// 		}
+// 		return
+// 	}
+// 	if hasExon && !hasIntron && exons > 0 {
+// 		st.Exon++
+// 		return
+// 	}
+// 	if hasIntron && !hasExon && introns > 0 {
+// 		st.Intron++
+// 		return
+// 	}
+// 	st.ExonIntron++
+// }
 
 // Collect collects genome coverage statistics from a sam.Record.
 func (s *CoverageStats) Collect(record *sam.Record, index *annotation.RtreeMap) {
@@ -102,13 +167,17 @@ func (s *CoverageStats) Collect(record *sam.Record, index *annotation.RtreeMap) 
 		mappingLocation.GetElements(&results, elements)
 	}
 	if record.IsSplit() {
-		updateCount(record, elements, &s.Split)
+		updateCount(record, elements, s.Split)
 	} else {
-		updateCount(record, elements, &s.Continuous)
+		updateCount(record, elements, s.Continuous)
 	}
 }
 
 // NewCoverageStats create a new instance of CoverageStats.
 func NewCoverageStats() *CoverageStats {
-	return &CoverageStats{}
+	return &CoverageStats{
+		Total:      make(ElementStats),
+		Continuous: make(ElementStats),
+		Split:      make(ElementStats),
+	}
 }
