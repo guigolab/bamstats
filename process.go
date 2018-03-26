3a4b6bcdf470321835f64c08cef0fe6e0a04cb86
Emilio Palumbo <emiliopalumbo@gmail.com>
2018-03-26 14:20:49 +0200

    Update processing for Stats refactoring

--- process.go
+++ process.go
@@ -19,29 +19,29 @@ func init() {
 
 // var wg sync.WaitGroup
 
-func worker(id int, in interface{}, out chan stats.StatsMap, index *annotation.RtreeMap, cfg *config.Config, wg *sync.WaitGroup) {
+func worker(id int, in interface{}, out chan stats.Map, index *annotation.RtreeMap, cfg *config.Config, wg *sync.WaitGroup) {
 	defer wg.Done()
 	logger := log.WithFields(log.Fields{
 		"worker": id,
 	})
 	logger.Debug("Starting")
 
-	sm := stats.NewStatsMap(true, (index != nil), cfg.Uniq)
+	sm := makeStatsMap(index, cfg)
 
-	collectStats(in, sm, index)
+	collectStats(in, sm)
 
 	logger.Debug("Done")
 
 	out <- sm
 }
 
-func collectStats(in interface{}, sm stats.StatsMap, index *annotation.RtreeMap) {
+func collectStats(in interface{}, sm stats.Map) {
 	switch in.(type) {
 	case chan *sam.Record:
 		c := in.(chan *sam.Record)
 		for record := range c {
 			for _, s := range sm {
-				s.Collect(record, index)
+				s.Collect(record)
 			}
 		}
 	case chan *sam.Iterator:
@@ -49,14 +49,14 @@ func collectStats(in interface{}, sm stats.StatsMap, index *annotation.RtreeMap)
 		for it := range iterators {
 			for it.Next() {
 				for _, s := range sm {
-					s.Collect(it.Record(), index)
+					s.Collect(it.Record())
 				}
 			}
 		}
 	}
 }
 
-func process(bamFile string, index *annotation.RtreeMap, cpu int, maxBuf int, reads int, uniq bool) (stats.StatsMap, error) {
+func process(bamFile string, index *annotation.RtreeMap, cpu int, maxBuf int, reads int, uniq bool) (stats.Map, error) {
 
 	var wg sync.WaitGroup
 
@@ -67,7 +67,7 @@ func process(bamFile string, index *annotation.RtreeMap, cpu int, maxBuf int, re
 	if err != nil {
 		return nil, err
 	}
-	statChan := make(chan stats.StatsMap, cpu)
+	statChan := make(chan stats.Map, cpu)
 	for i := 0; i < br.Workers; i++ {
 		id := i + 1
 		wg.Add(1)
@@ -90,13 +90,13 @@ func process(bamFile string, index *annotation.RtreeMap, cpu int, maxBuf int, re
 	return stat, nil
 }
 
-func waitProcess(st chan stats.StatsMap, wg *sync.WaitGroup) {
+func waitProcess(st chan stats.Map, wg *sync.WaitGroup) {
 	wg.Wait()
 	close(st)
 }
 
 // Process process the input BAM file and collect different mapping stats.
-func Process(bamFile string, anno string, cpu int, maxBuf int, reads int, uniq bool) (stats.StatsMap, error) {
+func Process(bamFile string, anno string, cpu int, maxBuf int, reads int, uniq bool) (stats.Map, error) {
 	var index *annotation.RtreeMap
 	if anno != "" {
 		log.Infof("Creating index for %s", anno)
@@ -114,7 +114,19 @@ func Process(bamFile string, anno string, cpu int, maxBuf int, reads int, uniq b
 	return stats, nil
 }
 
-func WriteOutput(output string, st stats.StatsMap) {
+func writeOutput(output string, st stats.Map) {
 	out := utils.NewOutput(output)
 	utils.OutputJSON(out, st)
+}
+
+func makeStatsMap(index *annotation.RtreeMap, cfg *config.Config) stats.Map {
+	m := make(stats.Map)
+	m.Add("general", stats.NewGeneralStats())
+	if index != nil {
+		m.Add("coverage", stats.NewCoverageStats(index, false))
+		if cfg.Uniq {
+			m.Add("coverageUniq", stats.NewCoverageStats(index, true))
+		}
+	}
+	return m
 }
