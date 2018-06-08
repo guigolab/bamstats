60c8c3205e058b839755640e6fc6fc3528aecc7a
Emilio Palumbo <emiliopalumbo@gmail.com>
2018-06-08 15:47:41 +0200

    Add preprocessing scripts to repo

---
+++ scripts/bamstats-preprocess-strand.sh
@@ -0,0 +1,10 @@
+# exons
+awk '$3 == "exon"' ${annotation} | sort -k1,1 -k4,4n | mergeBed -i stdin -c 7 -o distinct | awk -v OFS="\t" '$(NF+1)="exon"' > exons
+# genes
+awk '$3 == "gene"' ${annotation} | sort -k1,1 -k4,4n | mergeBed -i stdin -c 7 -o distinct | awk -v OFS="\t" '$(NF+1)="gene"' > genes
+# introns
+subtractBed -a genes -b exons | awk -v OFS="\t" '$(NF)="intron"' > introns
+# intergenic
+complementBed -i genes -g <(sort -k1,1 ${genome}) | awk -v OFS="\t" '$(NF+1)="intergenic"' > intergenic
+# all
+cat exons genes introns intergenic | sort -k1,1 -k2,2n > ${annotation}.elements.bed
