a6edb3af4a33b02b2610420705571387ce2afcae
Emilio Palumbo <emiliopalumbo@gmail.com>
2019-11-19 09:58:51 +0100

    Bump version to 0.3.5 for release

--- version.go
+++ version.go
@@ -5,7 +5,7 @@ import "fmt"
 // Constants for Major and Minor version numbers.
 const (
 	VersionNumber      = 0.3
-	MinorVersionNumber = 4
+	MinorVersionNumber = 5
 )
 
 // PreVersionString indicates wheather the program is a pre-release.
